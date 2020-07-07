6f5e007fe4027b1c4fc3d63a04381e37604fab28
azlan <mhdazlanabaziz@gmai.com>
2020-07-07 16:59:38 +0800

    Mongo second config

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+    require('dotenv').config({path: '.env'})
 }
 const express = require('express')
 const app = express()
@@ -13,7 +13,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true })
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
