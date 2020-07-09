10fafc3908384778f8e562ea156dccf66d386c78
azlan <mhdazlanabaziz@gmai.com>
2020-07-09 15:31:02 +0800

    initial author routes settled

--- routes/author.js
+++ routes/author.js
@@ -1,18 +1,60 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 //All Author Route
-router.get('/',(req,res)=>{
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !==''){
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors ,
+            searchOptions:req.query
+        })
+    } catch (error) {
+        res.redirect('/')
+    }
 })
 
 //New Author Route
-router.get('/new', (req,res)=> {
- res.render('authors/new')
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
 })
 
-router.post('/',(req,res) =>{
-    res.send('Create')
-})
+//Create Author Route without Async/Await
+// router.post('/',(req,res) =>{
+//     const author =new Author({
+//         name: req.body.name
+//     })
+//     author.save((err, newAuthor) =>{
+//         if(err){
+//             res.render('authors/new', {
+//                 author, 
+//                 errorMessage: 'Error Creating Author'
+//             }) 
+//         }else {
+//               res.redirect('authors')  
+//         }
+//     })
+//     //res.send(req.body.name)
+// })
 
+//Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    } catch (error) {
+        res.render('authors/new', {
+            author,
+            errorMessage: 'Error Creating Author'
+        })
+    }
+})
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path: '.env'})
+    require('dotenv').config({ path: '.env' })
 }
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/author')
@@ -13,15 +14,16 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb,extended:false' }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true,useNewUrlParser: true, useCreateIndex:true })
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', error => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
-//app.use('/authors',authorRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
