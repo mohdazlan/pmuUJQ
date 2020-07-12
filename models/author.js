53bcfd61014c7dca4de374da988b1dcc48d69839
azlan <mhdazlanabaziz@gmai.com>
2020-07-12 16:56:15 +0800

    Finish Author Routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Document = require('../models/document')
 
 const authorSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
     }
+})
+
+authorSchema.pre('remove',function(next) {
+ Document.find({author:this.id}, (err,document) => {
+     if(err){
+         next(err)
+     } else if(document.length > 0){
+         next(new Error('This author has books still '))
+     } else {
+         next()
+     }
+ })
 })
 
 module.exports = mongoose.model('Author',authorSchema)
--- routes/author.js
+++ routes/author.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Document = require('../models/document')
 
 //All Author Route
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.name != null && req.query.name !==''){
-        searchOptions.name = new RegExp(req.query.name,'i')
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
         const authors = await Author.find(searchOptions)
-        res.render('authors/index', { 
-            authors ,
-            searchOptions:req.query
+        res.render('authors/index', {
+            authors,
+            searchOptions: req.query
         })
     } catch (error) {
         res.redirect('/')
@@ -49,12 +50,71 @@ router.post('/', async (req, res) => {
     })
     try {
         const newAuthor = await author.save()
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
     } catch (error) {
         res.render('authors/new', {
             author,
             errorMessage: 'Error Creating Author'
-        })
+        }) 
     }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const documents = await Document.find({author:author.id}).limit(6).exec()
+        res.render('authors/show', {
+            author,
+            booksByAuthor: documents
+        })
+    } catch (error) {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', {author})
+    } catch (error) {
+        res.redirect('/authors')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+ let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+       await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch (error) {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.render('authors/new', {
+                author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+       
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id) 
+       await author.remove() 
+       res.redirect('/authors')
+    } catch (error) {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
+       
+    }
+})
+
 module.exports = router
